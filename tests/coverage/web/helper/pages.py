a10dd5394f5c7b1e6e95f1e87f361879c68824ff
Volodymyr Yahello <vyahello@gmail.com>
2020-04-01 14:51:28 +0300

    Implement 'edit current quote' test

--- tests/coverage/web/helper/pages.py
+++ tests/coverage/web/helper/pages.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from urequest.url import Address, HttpsUrl
 
 
@@ -13,6 +14,38 @@ class Page(ABC):
     @abstractmethod
     def loaded(self) -> bool:
         pass
+
+
+class HomePageUrl(Address):
+    """Home page url interface."""
+
+    def __init__(self) -> None:
+        self._home_url: Address = HttpsUrl(host="quote-quote.herokuapp.com")
+
+    def matcher(self) -> str:
+        return self._home_url.matcher()
+
+    def host(self) -> str:
+        return self._home_url.host()
+
+    def __str__(self) -> str:
+        return str(self._home_url)
+
+
+class EditPageUrl(Address):
+    """Home page url interface."""
+
+    def __init__(self, page_id: int) -> None:
+        self._edit_url: Address = HttpsUrl(host="quote-quote.herokuapp.com", path=f"edit/{page_id}")
+
+    def matcher(self) -> str:
+        return self._edit_url.matcher()
+
+    def host(self) -> str:
+        return self._edit_url.host()
+
+    def __str__(self) -> str:
+        return str(self._edit_url)
 
 
 class QuotesPage(Page):
@@ -34,7 +67,7 @@ class HomePage(Page):
 
     def __init__(self, browser: WebDriver) -> None:
         self._browser = browser
-        self._page: Page = QuotesPage(browser, HttpsUrl("quote-quote.herokuapp.com"))
+        self._page: Page = QuotesPage(browser, HomePageUrl())
 
     def navigate(self) -> None:
         self._page.navigate()
@@ -43,7 +76,7 @@ class HomePage(Page):
         return self._page.loaded()
 
     def title(self) -> str:
-        return self._browser.find_element_by_id("pageTitle").text
+        return self._browser.find_element_by_id("pageTitle").text.strip()
 
     def add_quote_button_displayed(self) -> bool:
         return self._browser.find_element_by_id("new").is_displayed()
@@ -53,3 +86,42 @@ class HomePage(Page):
 
     def login_button_displayed(self) -> None:
         return self._browser.find_element_by_id("login").is_displayed()
+
+    def message(self) -> str:
+        return self._browser.find_element_by_id("messages").text.strip()
+
+
+class EditPage(Page):
+    """Edit page interface."""
+
+    def __init__(self, browser: WebDriver, page_id: int) -> None:
+        self._browser = browser
+        self._page: Page = QuotesPage(browser, EditPageUrl(page_id))
+
+    def navigate(self) -> None:
+        self._page.navigate()
+
+    def loaded(self) -> bool:
+        return self._page.loaded()
+
+    def fill_quote(self, quote: str) -> None:
+        self._fill(quote, location_id="id_quote", clear=True)
+
+    def fill_author(self, author: str) -> None:
+        self._fill(author, location_id="id_author", clear=True)
+
+    def fill_source(self, source: str) -> None:
+        self._fill(source, location_id="id_source", clear=True)
+
+    def fill_cover(self, source: str) -> None:
+        self._fill(source, location_id="id_cover", clear=True)
+
+    def edit(self) -> HomePage:
+        self._browser.find_element_by_id("edit").click()
+        return HomePage(self._browser)
+
+    def _fill(self, value: str, location_id: str, clear: bool = False) -> None:
+        field: WebElement = self._browser.find_element_by_id(location_id)
+        if clear:
+            field.clear()
+        field.send_keys(value)
---
+++ tests/coverage/web/test_edit_quote.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
+from tests.coverage.web.helper.pages import EditPage
+from tests.markers import web  # noqa: I100
+
+pytestmark = web
+
+
+@pytest.fixture()
+def edit_page(browser: WebDriver) -> EditPage:
+    page: EditPage = EditPage(browser, page_id=3)
+    if not page.loaded():
+        page.navigate()
+    yield page
+
+
+def test_edit_current(edit_page: EditPage) -> None:
+    edit_page.fill_quote("If your dreams don't scare you, they aren't big enough")
+    edit_page.fill_author("Muhammad Ali")
+    edit_page.fill_source("https://www.blackfilm.com/read/2019/04/new-trailer-to-hbos-whats-my-name-muhammad-ali")
+    edit_page.fill_cover("https://www.blackfilm.com/read/wp-content/uploads/2019/04/Whats-My-Name.jpg")
+    assert edit_page.edit().message() == "Quote is updated"
